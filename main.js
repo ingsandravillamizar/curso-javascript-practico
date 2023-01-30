8cca04e9892a44eeb452396f345c2472650687aa
svillamizar <ingsandravillamizar@github.com>
2023-01-30 16:59:28 -0500

    clase 18

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-const menuEmail = document.querySelector('.navbar-email');
+
+
+const menuEmail = document.querySelector('.navbar-email'); //selecciona el elmento dode esta el email
 const desktopMenu = document.querySelector('.desktop-menu');
-menuEmail.addEventListener('click', toggleDesktopMenu);
+menuEmail.addEventListener('click', toggleDesktopMenu);  //al escuchar el evento click corre la funcon toggleDesktopMenu
 
 function toggleDesktopMenu() {
+    //quita o pone la clase inactive dependiendo si la tiene o no 
     desktopMenu.classList.toggle('inactive');
+    
 }
